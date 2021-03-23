ce50c5bb6bd3896b99343736fb54e29a17c6b397
Dario <darolisn454@gmail.com>
2021-03-23 16:50:21 -0600

    update

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -2,17 +2,35 @@ import React, { Component } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
-
-
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+  };
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
-      {/* <ImageLinkForm />
-      <FaceRecognition /> */}
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/* <FaceRecognition /> */}
       </div>
     );
   }
